f79b73bc5e370aa11a09d1b4eef901811fd0f2c3
blvu <013ayu@gmail.com>
2024-02-01 10:16:41 +0800

    add income form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length
 class ExpenseForm(FlaskForm):
 
     name = StringField('Name', 
-                       validators=[DataRequired()])
+                       validators=[DataRequired(), Length(min=1, max=64)])
     amount = DecimalField('Amount',
                           validators=[DataRequired()])
     date_spent = DateField('Date Spent',
@@ -22,9 +22,25 @@ class ExpenseForm(FlaskForm):
 class AccountForm(FlaskForm):
 
     name = StringField('Name',
-                       validators=[DataRequired()])
+                       validators=[DataRequired(), Length(min=1, max=64)])
     group = SelectField('Group',
                         validators=[DataRequired()],
                         choices=[("Cash", "Cash"), ("Bank", "Bank"), ("Card", "Card")])
     submit = SubmitField('Add Account')
+
+class IncomeForm(FlaskForm):
+
+    name = StringField('Name', 
+                       validators=[DataRequired(), Length(min=1, max=64)])
+    amount = DecimalField('Amount',
+                          validators=[DataRequired()])
+    date_received = DateField('Date Received',
+                           validators=[DataRequired()])
+    account = SelectField('Account', coerce=int,
+                           validators=[DataRequired()])
+    category = SelectField('Category',
+                           validators=[DataRequired()])
+    note = TextAreaField('Note',
+                         validators=[Length(max=60)])
+    submit = SubmitField('Add')
     
--- app/main/util.py
+++ app/main/util.py
@@ -1,76 +1,5 @@
 from flask_login import current_user
 
-
-def test_expenses():
-    expenses = [
-        {
-            'name': 'Cookie',
-            'amount': 120.00,
-            'date_spent': '2024-01-25',
-            'category': 'Food',
-            'paid_via': 'Cash',
-            'note': ''
-        },
-        {
-            'name': 'iPhone 15 Pro Max',
-            'amount': 12200.00,
-            'date_spent': '2024-01-25',
-            'category': 'Shopping',
-            'paid_via': 'Card',
-            'note': 'for sam'
-        },
-        {
-            'name': 'Electricity Bill',
-            'amount': 150.00,
-            'date_spent': '2024-01-26',
-            'category': 'Bills',
-            'paid_via': 'Online',
-            'note': 'utilities'
-        },
-        {
-            'name': 'Apartment Rent',
-            'amount': 1800.00,
-            'date_spent': '2024-01-27',
-            'category': 'Rent',
-            'paid_via': 'Bank Transfer',
-            'note': 'monthly'
-        },
-        {
-            'name': 'Weekend Getaway',
-            'amount': 500.00,
-            'date_spent': '2024-01-28',
-            'category': 'Other',
-            'paid_via': 'Card',
-            'note': 'relaxation'
-        },
-        {
-            'name': 'Grocery',
-            'amount': 80.00,
-            'date_spent': '2024-01-29',
-            'category': 'Food',
-            'paid_via': 'Cash',
-            'note': 'essentials'
-        },
-        {
-            'name': 'Student Loan',
-            'amount': 300.00,
-            'date_spent': '2024-01-30',
-            'category': 'Loan',
-            'paid_via': 'Online',
-            'note': 'monthly'
-        },
-        {
-            'name': 'Gym Membership',
-            'amount': 50.00,
-            'date_spent': '2024-01-31',
-            'category': 'Other',
-            'paid_via': 'Card',
-            'note': 'fitness'
-        }
-    ]
-
-    return expenses
-
 def get_categories():
 
     categories = [
@@ -83,6 +12,21 @@ def get_categories():
         'Travel',
         'Debt',
         'Loan',
+        'Other'
+    ]
+
+    return [(category, category) for category in categories]
+
+def get_income_categories():
+
+    categories = [
+        'Allowance',
+        'Salary',
+        'Passive',
+        'Bonus',
+        'Gift',
+        'Investment',
+        'Tips',
         'Other'
     ]
 

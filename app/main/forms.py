cee7b846131f3b4efbc089af6b6b35ce8988f35f
blvu <013ayu@gmail.com>
2024-02-02 23:04:23 +0800

    add record-form

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, SelectField, DecimalField, DateFie
 from wtforms.validators import DataRequired, Length
 
 
-class ExpenseForm(FlaskForm):
+class RecordForm(FlaskForm):
 
     name = StringField('Name', 
                        validators=[DataRequired(), Length(min=1, max=64)])
@@ -26,21 +26,9 @@ class AccountForm(FlaskForm):
     group = SelectField('Group',
                         validators=[DataRequired()],
                         choices=[("Cash", "Cash"), ("Bank", "Bank"), ("Card", "Card")])
+    
+    # initial balance
+    # description
+
     submit = SubmitField('Add Account')
-
-class IncomeForm(FlaskForm):
-
-    name = StringField('Name', 
-                       validators=[DataRequired(), Length(min=1, max=64)])
-    amount = DecimalField('Amount',
-                          validators=[DataRequired()])
-    date_received = DateField('Date Received',
-                           validators=[DataRequired()])
-    account = SelectField('Account', coerce=int,
-                           validators=[DataRequired()])
-    category = SelectField('Category',
-                           validators=[DataRequired()])
-    note = TextAreaField('Note',
-                         validators=[Length(max=60)])
-    submit = SubmitField('Add')
     
